97908f1856d5f79f17420dfb7b74eb5219a002e8
Ancientshield <ancientsword520@gmail.com>
2022-12-26 13:14:44 +0800

    Two Line Data-Binding

--- src/components/componentsA.vue
+++ src/components/componentsA.vue
@@ -1,7 +1,18 @@
 <template>
-  <h1>Hello world</h1>
+  <h1>資料單向綁定</h1>
+  {{ data }}
+  <h1>資料雙向綁定</h1>
+  <input type="text" v-model="text" />
+  <p>{{ text }}</p>
 </template>
 
-<script setup></script>
+<script setup>
+// 資料雙向綁定需要引入ref
+// import {ref} from "@vue/reactivity"
+import { ref } from "vue";
+// 資料驅動頁面
+const data = "Hello Vue";
+const text = ref("123");
+</script>
 
 <style></style>
