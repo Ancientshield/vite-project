dc35273a0b82f93914e0eff09137f782cae895cc
Ancientshield <ancientsword520@gmail.com>
2022-12-26 16:33:21 +0800

    init watchEffect

--- src/App.vue
+++ src/App.vue
@@ -10,6 +10,7 @@ import componentA from "./components/componentsA.vue";
 import vFor from "./components/vFor.vue";
 import Computed01 from "./components/Computed01.vue";
 import Computed02 from "./components/Computed02.vue";
+import watchEffect from "./components/watchEffect.vue";
 </script>
 
 <style></style>
---
+++ src/components/watchEffect.vue
@@ -0,0 +1,11 @@
+<template>
+  <input type="text" v-model="text" />
+</template>
+
+<script setup>
+import { ref } from "@vue/reactivity";
+</script>
+
+const text = ref("")
+
+<style></style>
