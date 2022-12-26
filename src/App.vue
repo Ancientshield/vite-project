46bb7a01d5cb205fa5ad27ebc2628311cb69a98a
Ancientshield <ancientsword520@gmail.com>
2022-12-26 14:40:52 +0800

    v-for

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,11 @@
 <template>
+  <vFor />
   <componentA />
 </template>
 
 <script setup>
 import componentA from "./components/componentsA.vue";
+import vFor from "./components/vFor.vue";
 </script>
 
 <style></style>
---
+++ src/components/vFor.vue
@@ -0,0 +1,41 @@
+<template>
+  <div>
+    <!-- 使用 :key="" 綁定，會重新創建 element 觸發 render -->
+    <h1 class="test-cls" :key="test">{{ test }}</h1>
+  </div>
+  <!-- for d in dataList, 用v-bind:key綁定 key -->
+  <!-- <p v-for="(d, i) in dataList" v-bind:key="i">{{ d }}</p> -->
+
+  <!-- 可簡寫 :key="" 綁定每一個 array 值 -->
+  <p v-for="(d, i, index) in dataList" :key="d">
+    <span v-if="i !== 'key3'"> index:{{ index }} {{ i }}: {{ d }} </span>
+  </p>
+</template>
+
+<script setup>
+import { ref } from "vue";
+const test = ref("test");
+const dataList = { key1: "a", key2: "b", key3: "c", key4: "d" };
+
+setTimeout(() => {
+  test.value = "apple";
+}, 2000);
+</script>
+
+<style>
+.test-cls {
+  font-size: 40px;
+  animation: openIn 1s ease-in-out;
+}
+
+@keyframes openIn {
+  0% {
+    opacity: 0;
+    transform: translateY(20px);
+  }
+  100% {
+    opacity: 1;
+    transform: translateY(0);
+  }
+}
+</style>
